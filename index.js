6160a7b5523449dd80464746d8a4be182d27fa7d
Derek Miranda <derek.miranda@ff0000.com>
2019-05-10 11:41:37 -0700

    feature: start on main file

--- index.js
+++ index.js
@@ -1,25 +1,15 @@
-const fsPr = require('fs').promises
-const path = require('path')
-const argv = require('minimist')(process.argv.slice(2))
-const puppeteer = require('puppeteer')
+#!/usr/bin/env node
+const fsPr = require("fs").promises;
+const path = require("path");
+const util = require("util");
+const puppeteer = require("puppeteer");
+const argv = require("yargs")
+  .array("b")
+  .array("branches").argv;
 
-async function main(buildTemplate) {
-
+async function main() {
+  const adPath = argv._[0];
+  const branches = argv.b || argv.branches;
 }
 
-async function getScreenshot() {
-  const browser = await puppeteer.launch()
-  const page = await browser.newPage()
-  await page.goto(/* BT link to 3-traffic */)
-  await page.screenshot({ path: '' })
-
-  await browser.close()
-}
-
-const buildTemplate = argv[0]
-
-if (!buildTemplate) {
-  throw new Error('Please provide a build template as the first arg')
-}
-
-main(buildTemplate)
+main();
